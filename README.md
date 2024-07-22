bb668cf35ba6ed1acc532c2ffefd897fcc19bae6
Taaha-Tariq <taahatariq2005@gmail.com>
2024-07-22 11:50:23 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project #
+This is a **portfolio website** to showcase my skills and qualifications and the relevant contact information.
+
+## Description: ##
+ The main page is *responsive* and you can change colors by pressing on the image for desktops and tablets and revert the changes to the original style by passing the mouse over the title.
+ 
+ The website is currently live at https://taaha-tariq.github.io/Portfolio/.
+
+
+ 
---
+++ Resources/Js/rand.js
@@ -0,0 +1,66 @@
+const targetElement = document.querySelector('.img-cnt');
+const heading = document.querySelector('.title');
+const button = document.querySelector('.button');
+const span = document.querySelector('span');
+const body = document.body;
+const list = document.querySelectorAll('a');
+const img = document.querySelectorAll('img');
+const html = document.querySelector('html');
+
+function randNumGen () {
+    return Math.floor(Math.random() * 256);
+}
+
+function changeColor () {
+    const num1 = randNumGen();
+    const num2 = randNumGen();
+    const num3 = randNumGen();
+    if(num1 !== 0 || num2 !== 0 || num3 !== 0) {
+        heading.style.color = `rgb(${num1}, ${num2}, ${num3})`;
+        span.style.color = `rgb(${num1}, ${num2}, ${num3})`;
+        button.style.backgroundColor = `rgba(${num1}, ${num2}, ${num3}, 0.85)`;
+        body.style.backgroundImage = `linear-gradient(black 50%, rgb(${num1}, ${num2}, ${num3}))`;
+        html.style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`;
+        for (let i = 0; i < list.length - 1; i++) {
+            list[i].addEventListener('mouseover', function () {
+                list[i].style.color = `rgb(${num1}, ${num2}, ${num3})`;
+            })
+            list[i].addEventListener('mouseout', function () {
+                list[i].style.color = 'white';
+        })
+        }
+        for (let i = 0; i < img.length; i++) {
+            img[i].style.border = `1px solid rgb(${num1}, ${num2}, ${num3}`
+        }
+        button.addEventListener('mouseover', function () {
+            button.style.backgroundColor = `rgb(${num1}, ${num2}, ${num3})`
+        })
+        button.addEventListener('mouseout', function () {
+            button.style.backgroundColor = `rgba(${num1}, ${num2}, ${num3}, 0.85)`;
+        })
+    }
+}
+
+targetElement.addEventListener('click', changeColor);
+
+heading.addEventListener('mouseover', function () {
+    heading.style.color = 'white';
+    body.style.backgroundImage = 'linear-gradient(rgb(0, 0, 0) 50%, rgba(57, 222, 230, 0.999))';
+    span.style.color = 'rgba(57, 222, 230, 0.999)';
+    button.style.backgroundColor = 'rgba(37, 184, 192, 0.999)';
+    html.style.backgroundColor = 'rgba(57, 222, 230, 0.999)';
+    for (let i = 0; i < list.length - 1; i++) {
+        list[i].addEventListener('mouseover', function () {
+            list[i].style.color = 'rgba(57, 222, 230, 0.999)'
+        })
+    }
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.border = '1px solid rgba(57, 222, 230, 0.999)'
+    }
+    button.addEventListener('mouseover', function () {
+        button.style.backgroundColor = 'rgba(57, 222, 230, 0.999)';
+    })
+    button.addEventListener('mouseout', function () {
+        button.style.backgroundColor = 'rgba(37, 184, 192, 0.999)';
+    })
+});
